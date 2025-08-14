2203c21ba7baf1ef633ee730ec2414dbc39a3250
Rakshit Dhoundiyal <151960863+SlicRik8@users.noreply.github.com>
2025-08-15 01:57:54 +0530

    initial commit

---
+++ StockPhotoGenerator/src/main/java/com/StockPhotoGenerator/StockPhotoGenerator/StockPhotoGeneratorApplication.java
@@ -0,0 +1,15 @@
+package com.StockPhotoGenerator.StockPhotoGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPhotoGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPhotoGeneratorApplication.class, args);
+	}
+
+}
+
+
---
+++ StockPhotoGenerator/src/test/java/com/StockPhotoGenerator/StockPhotoGenerator/StockPhotoGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StockPhotoGenerator.StockPhotoGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPhotoGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
